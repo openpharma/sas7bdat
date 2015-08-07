27c1fcefc2fbb169f69977693c81f2c5d1b15a96
Jared Hobbs <jared@pyhacker.com>
2015-08-07 09:09:25 -0700

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (2, 6):
 
 
 setup(name='sas7bdat',
-      version='2.0.4',
+      version='2.0.5',
       author='Jared Hobbs',
       author_email='jared@pyhacker.com',
       license='MIT',
