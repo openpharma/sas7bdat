3f94bc2a106b22139f16c462ef9de989599c0658
Jared Hobbs <jared@pyhacker.com>
2018-11-04 22:21:17 -0700

    revert setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,17 @@ from __future__ import division, absolute_import, print_function,\
 import sys
 from distutils.core import setup
 
+try:
+    from pypandoc import convert_file
+except ImportError:
+    print('warning: pypandoc not found, could not convert Markdown to RST.')
+
+    def convert_file(filename, to):
+        with open(filename, 'r') as f:
+            data = f.read()
+        return data
+
+
 if sys.version_info < (2, 6):
     print("Sorry, this module only works on 2.6+, 3+")
     sys.exit(1)
@@ -16,8 +27,7 @@ setup(name='sas7bdat',
       license='MIT',
       url='https://bitbucket.org/jaredhobbs/sas7bdat',
       description='A sas7bdat file reader for Python',
-      long_description=open('README.md', 'r').read(),
-      long_description_content_type='text/markdown',
+      long_description=convert_file('README.md', 'rst'),
       py_modules=['sas7bdat'],
       scripts=['scripts/sas7bdat_to_csv'],
       install_requires=['six>=1.8.0'],
